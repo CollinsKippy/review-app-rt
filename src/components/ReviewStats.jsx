b7cae85f118c23f838d1693c944a86aac7dd16c0
CK <collins@analence.com>
2023-01-09 22:04:57 +0300

    stats PropTypes

--- src/components/ReviewStats.jsx
+++ src/components/ReviewStats.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ReviewStats(props) {
   const { reviews } = props;
@@ -15,5 +16,9 @@ function ReviewStats(props) {
     </div>
   );
 }
+
+ReviewStats.propTypes = {
+  reviews: PropTypes.array.isRequired,
+};
 
 export default ReviewStats;
